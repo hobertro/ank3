308785764438240fc7b59996f1f3102f0cccd796
hobertro <boho527@gmail.com>
2019-08-06 22:35:06 -0700

    First commit

---
+++ lib/ank3/version.rb
@@ -0,0 +1,3 @@
+module Ank3
+  VERSION = "0.1.0"
+end
---
+++ spec/ank3_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Ank3 do
+  it "has a version number" do
+    expect(Ank3::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
