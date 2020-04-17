5fe7635db092832657bd21ffcb7e9c30512cbe83
hobertro <boho527@gmail.com>
2020-04-16 21:04:35 -0700

    reverse order of arguments

--- lib/ank3/cli.rb
+++ lib/ank3/cli.rb
@@ -6,8 +6,8 @@ module Ank3
     end
 
     desc "generate_boilerplate", "Generate a boilerplate YAML file"
-    def generate_boilerplate(number = 1, file_name = "ank3.yml")
-      Ank3::Collection.generate_boilerplate_file(number, file_name)
+    def generate_boilerplate(file_name = "ank3.yml", number = 1)
+      Ank3::Collection.generate_boilerplate_file(file_name, number)
     end
   end
 end
